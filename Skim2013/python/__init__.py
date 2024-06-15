0541dc674bc81979ec6f632065146ec4111ab34e
Wei <liwei810812@gmail.com>
2024-06-15 20:09:00 +0200

    Clean up

--- Skim2013/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/RiceHIG/Skim2013/',1)[0])+'/cfipython/slc7_amd64_gcc820/RiceHIG/Skim2013')
